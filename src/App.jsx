055a515278d8334c1187e26bf23addf2c071ad1f
Hrithik <slumdog4321@gmail.com>
2023-08-11 00:25:03 +0530

    Responsive Navbar Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,111 @@
+import Spline from "@splinetool/react-spline";
+import { useState } from "react";
+import { IoMenu } from "react-icons/io5";
+
 function App() {
+  const [isActive, setIsActive] = useState(false);
   return (
     <>
-      <div className="text-4xl italic text-center">Let's Begin Crafting</div>
+      <div className="flex flex-col w-screen min-h-screen justify-center items-center relative bg-primary pb-10">
+        {/* Navigation */}
+        <nav className="w-full inset-x-0 fixed z-50 flex justify-center items-center px-6 top-2">
+          <div className="w-full md:w-880 bg-navBar p-4 flex items-center rounded-2xl">
+            <p className="text-lg text-slate-200 font-medium">Hrithik Dutta</p>
+            <div className="hidden md:flex items-center gap-6 ml-6 flex-1">
+              <a
+                href="#home"
+                className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in"
+              >
+                Home
+              </a>
+              <a
+                href="#about"
+                className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in"
+              >
+                About
+              </a>
+              <a
+                href="#projects"
+                className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in"
+              >
+                Projects
+              </a>
+              <a
+                href="#contacts"
+                className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in"
+              >
+                Contact
+              </a>
+              <a
+                href="#"
+                className="ml-auto text-base text-textBase font-medium hover:text-slate-100 cursor-pointer border border-textBase px-2 py-1 rounded-xl hover:border-gray-100 duration-100 ease-in"
+              >
+                Download
+              </a>
+            </div>
+            <div
+              className="block md:hidden  ml-auto cursor-pointer text-white"
+              onClick={() => setIsActive(!isActive)}
+            >
+              <IoMenu />
+            </div>
+            {isActive && (
+              <div
+                // initial={{ opacity: 0, scale: 0.5 }}
+                // animate={{ opacity: 1, scale: 1.1 }}
+                // exit={{ opacity: 0, scale: 0.5 }}
+                // transition={{ delay: "0.1s", type: "spring" }}
+                className="p-4 w-275 bg-navBar rounded-lg fixed top-24 right-16 flex flex-col items-center justify-evenly gap-6"
+              >
+                <a
+                  href="#home"
+                  className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in"
+                  onClick={() => setIsActive(false)}
+                >
+                  Home
+                </a>
+                <a
+                  href="#about"
+                  className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in"
+                  onClick={() => setIsActive(false)}
+                >
+                  About
+                </a>
+                <a
+                  href="#projects"
+                  className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in"
+                  onClick={() => setIsActive(false)}
+                >
+                  Projects
+                </a>
+                <a
+                  href="#contacts"
+                  className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in"
+                  onClick={() => setIsActive(false)}
+                >
+                  Contact
+                </a>
+                <a
+                  href="#"
+                  className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer border border-textBase px-2 py-1 rounded-xl hover:border-gray-100 duration-100 ease-in"
+                  onClick={() => setIsActive(false)}
+                >
+                  Download
+                </a>
+              </div>
+            )}
+          </div>
+        </nav>
+        <div className="relative" id="home">
+          {/* <Spline scene="https://prod.spline.design/Gj81hfRZg1qEbrmb/scene.splinecode" /> */}
+          <Spline scene="https://prod.spline.design/oo6IxFu8UDjFUmjD/scene.spline" />
+          <div className="absolute bottom-10 w-full flex justify-center items-center">
+            <div className="shadow-md p-4 flex items-center justify-center bg-zinc-900 rounded-3xl ">
+              <p className="text-white">Press and drag to orbit</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
