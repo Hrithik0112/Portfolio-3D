bce201d7812b6d15d4f5c3446d4dcd0600685602
Hrithik <slumdog4321@gmail.com>
2023-08-11 16:14:04 +0530

    Project Section Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import Spline from "@splinetool/react-spline";
 import { useState } from "react";
-import { IoMenu } from "react-icons/io5";
+import { IoMenu, IoLogoGithub } from "react-icons/io5";
 import Pfp from "./assets/Avater.png";
+import { Projects } from "./data";
 
 function App() {
   const [isActive, setIsActive] = useState(false);
@@ -144,6 +145,39 @@ function App() {
                 </span>
               </button>
             </div>
+          </section>
+          {/* React vertical time line to be added when i got some experience */}
+          {/* Projects Section */}
+          <section className="flex flex-wrap items-center justify-evenly my-24 gap-4" id="projects">
+            {Projects &&
+              Projects.map((n, i) => (
+                <div
+                  key={n.id}
+                  className="border border-zinc-800 rounded-md p-2 min-w-[275px] md:max-w-[275px] hover:border-zinc-600 duration-100 ease-in-out"
+                >
+                  <p className="text-lg text-textBase font-medium uppercase">
+                    {n.name.length > 25 ? `${n.name.slice(0, 25)}...` : n.name}
+                  </p>
+
+                  <img
+                    src={n.imageSrc}
+                    className="w-full h-full object-cover rounded-md my-4"
+                    alt=""
+                  />
+
+                  <div className="flex flex-1 items-center justify-between">
+                    <p className="text-lg text-gray-300">
+                      Technologies
+                      <span className="block text-sm text-gray-500">{n.techs}</span>
+                    </p>
+                    <a href={n.github}>
+                      <div>
+                        <IoLogoGithub className="text-textBase text-3xl cursor-pointer" />
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              ))}
           </section>
         </main>
       </div>
---
+++ src/data.js
@@ -0,0 +1,109 @@
+import {
+  IoCodeWorking,
+  IoLogoGithub,
+  IoLogoYoutube,
+  IoLogoTwitter,
+  IoLogoLinkedin,
+  IoLogoWhatsapp,
+} from "react-icons/io5";
+import img1 from "./assets/fooddeliveryapp.png";
+import img2 from "./assets/socialmedia.png";
+import img3 from "./assets/google-map.png";
+import img4 from "./assets/pinterest.png";
+import img5 from "./assets/videoblog.png";
+import img6 from "./assets/whatsappui.png";
+
+// mock data for experience
+
+// export const Experience = [
+//   {
+//     id: 1,
+//     date: "2016 - present",
+//     iconsSrc: <IoCodeWorking />,
+//     title: "Creative Director",
+//     location: "Doha, Qatar",
+//     description:
+//       "Creative Direction, User Experience, Visual Design, Project Management, Team Leading",
+//   },
+//   {
+//     id: 2,
+//     date: "2014 - 2016",
+//     iconsSrc: <IoCodeWorking />,
+//     title: "Creative Director",
+//     location: "Doha, Qatar",
+//     description:
+//       "Creative Direction, User Experience, Visual Design, Project Management, Team Leading",
+//   },
+//   {
+//     id: 3,
+//     date: "2012 - 2014",
+//     iconsSrc: <IoCodeWorking />,
+//     title: "Creative Director",
+//     location: "Doha, Qatar",
+//     description:
+//       "Creative Direction, User Experience, Visual Design, Project Management, Team Leading",
+//   },
+//   {
+//     id: 4,
+//     date: "2011 - 2012",
+//     iconsSrc: <IoCodeWorking />,
+//     title: "Creative Director",
+//     location: "Doha, Qatar",
+//     description:
+//       "Creative Direction, User Experience, Visual Design, Project Management, Team Leading",
+//   },
+//   {
+//     id: 5,
+//     date: "2010 - 2011",
+//     iconsSrc: <IoCodeWorking />,
+//     title: "Creative Director",
+//     location: "Doha, Qatar",
+//     description:
+//       "Creative Direction, User Experience, Visual Design, Project Management, Team Leading",
+//   },
+// ];
+
+export const Projects = [
+  {
+    id: 1,
+    name: "Food Delivery UI Mockup",
+    imageSrc: img1,
+    techs: "React Js, Redux, Material UI",
+    github: "#",
+  },
+  {
+    id: 2,
+    name: "Full Stack Social Media App ",
+    imageSrc: img2,
+    techs: "React Js, Sanity, Tailwind CSS",
+    github: "#",
+  },
+  {
+    id: 3,
+    name: "Travel Advisor",
+    imageSrc: img3,
+    techs: "React Js, Chakra UI, Google API",
+    github: "#",
+  },
+  {
+    id: 4,
+    name: "Pintrest UI Clone 2.0 ",
+    imageSrc: img4,
+    techs: "React Js,  Material UI",
+    github: "#",
+  },
+  {
+    id: 5,
+    name: "Own Video Blog",
+    imageSrc: img5,
+    techs: "React Js, Chara UI, Firebase",
+    github: "#",
+  },
+  {
+    id: 6,
+    name: "Whatsapp UI Clone",
+    imageSrc: img6,
+    techs: "NextJs, Firebase Auth",
+    github: "#",
+  },
+];
