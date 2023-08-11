176587b7242ae00b2a6bf606f7e1d8e0a3f7dcf0
Hrithik <slumdog4321@gmail.com>
2023-08-11 11:29:08 +0530

    About section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Spline from "@splinetool/react-spline";
 import { useState } from "react";
 import { IoMenu } from "react-icons/io5";
+import Pfp from "./assets/Avater.png";
 
 function App() {
   const [isActive, setIsActive] = useState(false);
@@ -105,6 +106,46 @@ function App() {
             </div>
           </div>
         </div>
+        {/* main section */}
+        <main className="w-[80%] mt-4">
+          <section className="w-full grid grid-cols-1 md:grid-cols-2 gap-4 my-24" id="about">
+            <div className="w-full h-420 flex items-center justify-center">
+              <div className="w-275 h-340 relative bg-emerald-200 rounded-md">
+                <img
+                  src={Pfp}
+                  alt="Profile Picture"
+                  className="w-full h-full absolute -right-4 top-4 object-cover rounded-lg drop-shadow-2xl bg-black"
+                />
+              </div>
+            </div>
+            <div className="w-full h-420 flex flex-col items-center justify-center ">
+              <p className="text-lg text-textBase text-center">
+                As an aspiring Frontend and Fullstack Developer student, my passion lies in crafting
+                immersive digital experiences. Eagerly seeking opportunities to merge creativity
+                with technical prowess, I strive to bridge user-centric design with seamless
+                functionality. With a commitment to continuous learning, I'm poised to contribute
+                innovative solutions and enhance user engagement in the dynamic world of web
+                development.
+              </p>
+
+              <button
+                class="w-full md:w-auto relative mt-6 inline-flex items-center justify-center 
+              p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group 
+              bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400
+               group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4
+                focus:ring-green-200 dark:focus:ring-green-800 hover:shadow-lg 
+                hover:shadow-teal-500/50 hover:dark:shadow-lg hover:dark:shadow-teal-800/80"
+              >
+                <span
+                  class="w-full md:w-auto relative px-5 py-2.5 transition-all ease-in duration-75
+                 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0"
+                >
+                  Download
+                </span>
+              </button>
+            </div>
+          </section>
+        </main>
       </div>
     </>
   );
